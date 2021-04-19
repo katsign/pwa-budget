98b796a2c886fcfc906b7b73e06b53992952229e
Kat Buchanan <katrbuch@gmail.com>
2021-04-18 20:23:08 -0500

    edit connection

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
   useFindAndModify: false
 });
 
